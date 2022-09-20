f2b255673d2789ba666621266d3912a4ad33d5c5
Christian Green <hoolio.suare@gmail.com>
2022-09-19 20:02:56 -0600

    addedusershow

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/shared/home/Home';
-import Nomatch from '/components/shared/Nomatch';
+import Nomatch from './components/shared/Nomatch';
 import MainNavbar from './components/shared/MainNavbar';
+import Footer from './components/shared/Footer';
+import Users from './components/users/Users';
+import UserShow from './components/users/UserShow';
 
 const App = () => (
   <>
+    <MainNavbar />
     <Routes>
       <Route path='/' element={ <Home /> } />
+      <Route path='/users' element={ <Users /> } />
+      <Route path='/users/:id' element={ <UserShow /> } />
+      <Route path='/*' element={ <Nomatch /> } />
     </Routes>
+    <Footer />
   </>
 )
 
