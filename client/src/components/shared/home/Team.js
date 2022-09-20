a4588b205f0d8aca26803a43d6af3425027c2cae
Christian Green <hoolio.suare@gmail.com>
2022-09-19 21:16:44 -0600

    filled out team members

--- client/src/components/shared/home/Team.js
+++ client/src/components/shared/home/Team.js
@@ -6,33 +6,33 @@ const Team = () => (
     <Row>
       <Col md='3' sm='12'>
         <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="https://images.unsplash.com/photo-1663529695671-2134387a4827?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1015&q=80" />
+          <Card.Img variant="top" src="https://images.unsplash.com/photo-1612633201772-9a8f3df6bbf6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Y2FwdGFpbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=700&q=60" />
           <Card.Body>
-            <Card.Title>Bob Smith</Card.Title>
+            <Card.Title>Ricky Bobby</Card.Title>
           </Card.Body>
         </Card>
       </Col>
       <Col md='3' sm='12'>
         <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="https://images.unsplash.com/photo-1663529695671-2134387a4827?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1015&q=80" />
+          <Card.Img variant="top" src="https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8bGVhZGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=700&q=60" />
           <Card.Body>
-            <Card.Title>Bob Smith</Card.Title>
+            <Card.Title>Jennifer Harwood</Card.Title>
           </Card.Body>
         </Card>
       </Col>
       <Col md='3' sm='12'>
         <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="https://images.unsplash.com/photo-1663529695671-2134387a4827?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1015&q=80" />
+          <Card.Img variant="top" src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjl8fHBlcnNvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=700&q=60" />
           <Card.Body>
-            <Card.Title>Bob Smith</Card.Title>
+            <Card.Title>Robert Talisman</Card.Title>
           </Card.Body>
         </Card>
       </Col>
       <Col md='3' sm='12'>
         <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="https://images.unsplash.com/photo-1663529695671-2134387a4827?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1015&q=80" />
+          <Card.Img variant="top" src="https://images.unsplash.com/photo-1554727242-741c14fa561c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=60" />
           <Card.Body>
-            <Card.Title>Bob Smith</Card.Title>
+            <Card.Title>Amanda Tawn</Card.Title>
           </Card.Body>
         </Card>
       </Col>
