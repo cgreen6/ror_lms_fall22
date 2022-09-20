ae4f6f4698ce0bacc0556962c2e84f15538a149d
Christian Green <hoolio.suare@gmail.com>
2022-09-19 19:33:52 -0600

    updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
-
+import Home from './components/shared/home/Home';
+import Nomatch from '/components/shared/Nomatch';
+import MainNavbar from './components/shared/MainNavbar';
 
 const App = () => (
   <>
-  
+    <Routes>
+      <Route path='/' element={ <Home /> } />
+    </Routes>
   </>
 )
 
